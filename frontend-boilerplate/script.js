54a72ea394a376d2b5a3b68e09c970b738692ca8
ashaunthomas <ashaunthomas@siu.edu>
2017-09-17 20:35:19 -0500

    object classes setup, basic instantiation

--- frontend-boilerplate/script.js
+++ frontend-boilerplate/script.js
@@ -8,6 +8,14 @@
 	arcTo(x1,y1,x2,y2,radius)
 */ 
 
+const GAMELOOP_TIME = 16;
+const UP_KEY = 119;
+const DOWN_KEY = 115;
+const SHOOT_KEY = 13;
+var upIsPressed = false;
+var downIsPressed = false;
+var shootIsPressed = false;
+
 class Shape {
 	constructor(c,x,y) {
 		this.ctx = c;
@@ -28,7 +36,26 @@ class Rectangle extends Shape {
 		this.width = w;
 		this.height = h;
 	}
+	getWidth() { return this.width; }
+	getHeight() { return this.height; }
 	draw() { this.ctx.fillRect(this.xPos,this.yPos,this.width,this.height); }
+}
+
+class Cannon extends Rectangle {
+	constructor(c,x,y,w,h) {
+		super(c,x,y);
+		this.width = w;
+		this.height = h;
+		this.degrees = 0;
+	}
+	move(degrees) {
+		this.ctx.save();
+		this.degrees+=1;
+		this.ctx.rotate(this.degrees*Math.PI/180);
+		this.ctx.xPos-=2;
+		this.ctx.yPos-=3;
+		
+	}
 }
 
 class Circle extends Shape {
@@ -52,15 +79,44 @@ class Circle extends Shape {
 	}	
 }
 
+class CannonBall extends Circle {
+	constructor(c,x,y) {
+		super(c,x,y);
+		this.radius = 2;
+		this.speed = 1;
+		this.xPos_Saved = x;
+	}
+	reset() {
+		this.xPos = this.xPos_Saved;
+	}
+}
+
+function CanvasClear(canvas,context) { c.clearRect(0,0,canvas.width,canvas.height); }
+
 
 window.onload = function() {
 	var canvas = document.getElementById('screen');
 	var ctx = canvas.getContext('2d');
-	var wheel = new Circle(ctx,10,295,5);
-	wheel.draw('fill');
-	var cannon = new Rectangle(ctx,10,290,15,5);
-	cannon.draw();
+	//var wheel = new Circle(ctx,13,297,3);
+	var cannon = new Cannon(ctx,canvas.width/2,canvas.height/2,20,20);
+	
+	window.onkeypress = function(e) {
+		if (e.keyCode == UP_KEY) { 
+			cannon.move(10);
+		}
+		else if (e.keyCode == DOWN_KEY) { 
+			cannon.setY(cannon.getY()+5);
+		}
+		else if (e.keyCode == SHOOT_KEY) 
+			shootIsPressed = true;
+	};
+	
+	
+	setInterval(function() {
+		ctx.clearRect(0,0,canvas.width,canvas.height);
+		cannon.draw();
+		//wheel.draw('fill');
+	}, GAMELOOP_TIME);
 };
-
 
 
